4aa341dbe154fe2fea9e35069d5fc370acf0160e
james <guoqiang5277@163.com>
2020-05-25 16:23:39 +0800

    [修复]修复安装的时候对php7.x的兼容

--- bo-blog/install/db.php
+++ bo-blog/install/db.php
@@ -9,40 +9,42 @@ Copyright (c) Bob Shen й?
 In memory of my university life
 ------------------------------------------------------- */
 
-if (!function_exists("mysql_connect")) {
+if (!function_exists("mysqli_connect")) {
 	die ("Your server does not seem to support MySQL, so Bo-Blog 2.x can not run at your server.");
 }
 
 function db_connect($dbhost, $dbuser, $dbpw, $dbname='') {
-	if(!@mysql_connect($dbhost, $dbuser, $dbpw)) {
-		db_halt('Can not connect to MySQL server');
+    $con = mysqli_connect($dbhost, $dbuser, $dbpw);
+	if(!$con) {
+		db_halt($con,'Can not connect to MySQL server');
 	}
 	if (!empty($dbname)) {
-		$a_result=mysql_select_db($dbname);
+		$a_result=mysqli_select_db($con,$dbname);
 		/*if ($a_result) {
 			if (mysql_get_server_info()>='4.1.0') mysql_query("SET NAMES 'utf8'");
 		}*/
-		return $a_result;
+		return $con;
 	}
 }
 
-function db_select_db($dbname) {
-	$a_result=mysql_select_db($dbname);
+function db_select_db($con,$dbname) {
+	$a_result=mysqli_select_db($con,$dbname);
 	if ($a_result) {
-		if (mysql_get_server_info()>='4.1.0') mysql_query("SET NAMES 'utf8'");
+		if (mysql_geti_server_info($con)>='4.1.0') mysqli_query($con,"SET NAMES 'utf8'");
 	}
 	return $a_result;
 }
 
-function db_fetch_array($query, $result_type = MYSQL_ASSOC) {
-	return mysql_fetch_array($query, $result_type);
+//mysqli_fetch_array该函数不需要加链接
+function db_fetch_array($query, $result_type = MYSQLI_ASSOC) {
+	return mysqli_fetch_array($query, $result_type);
 }
 
-function db_query($sql, $silence = 0) {
+function db_query($con,$sql, $silence = 0) {
 	global $querynum;
-	$query = mysql_query($sql);
+	$query = mysqli_query($con,$sql);
 	if(!$query && !$silence) {
-		db_halt('MySQL Query Error', $sql);
+		db_halt($con,'MySQL Query Error', $sql);
 	}
 	$querynum++;
 	return $query;
@@ -59,54 +61,52 @@ function db_unbuffered_query($sql, $silence = 0) {
 	return $query;
 }
 
-function db_affected_rows() {
-	return mysql_affected_rows();
+function db_affected_rows($con) {
+	return mysqli_affected_rows($con);
 }
 
-function db_error() {
-	return mysql_error();
+function db_error($con) {
+	return mysqli_error($con);
 }
 
-function db_errno() {
-	return mysql_errno();
+function db_errno($con) {
+	return mysqli_errno($con);
 }
 
-function result($query, $row) {
-	$query = @mysql_result($query, $row);
-	return $query;
-}
-
+//mysqli_num_rows不需要转化
 function db_num_rows($query) {
-	$query = mysql_num_rows($query);
+	$query = mysqli_num_rows($query);
 	return $query;
 }
-
+//mysqli_num_fields 不需要转化
 function db_num_fields($query) {
-	return mysql_num_fields($query);
+	return mysqli_num_fields($query);
 }
 
+//mysqli_free_result 不需要转化
 function db_free_result($query) {
-	return mysql_free_result($query);
+	return mysqli_free_result($query);
 }
 
-function db_insert_id() {
-	$id = mysql_insert_id();
+function db_insert_id($con) {
+	$id = mysqli_insert_id($con);
 	return $id;
 }
 
+//mysqli_fetch_row 不需要转化
 function db_fetch_row($query) {
-	$query = mysql_fetch_row($query);
+	$query = mysqli_fetch_row($query);
 	return $query;
 }
 
-function db_close() {
-	mysql_close();
+function db_close($con) {
+	mysqli_close($con);
 }
 
-function db_halt($message = '', $sql = '') {
+function db_halt($con,$message = '', $sql = '') {
 	global $errmsg;
-	$dberror = db_error();
-	$dberrno = db_errno();
+	$dberror = db_error($con);
+	$dberrno = db_errno($con);
 	if($sql) $errmsg .= "<b>SQL</b>: ".htmlspecialchars($sql)."<br>";
 	$errmsg .= "<b>Error</b>:  $dberror<br>";
 	$errmsg .= "<b>Errno. </b>:  $dberrno<br>";
