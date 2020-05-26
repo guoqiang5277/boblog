cb40759c4cda8640117f9a0cae73845c1efb4c21
james <guoqiang5277@163.com>
2020-05-26 18:16:38 +0800

    更新缓存连接

--- bo-blog/install/copy/cache_links.php
+++ bo-blog/install/copy/cache_links.php
@@ -1,5 +1,5 @@
 <?PHP
 $linksshow=<<<eot
-<ul><li><strong>{langcopy0}</strong></li><li class="indent"><a href="http://www.bo-blog.com" target="_blank" title="Bo-Blog - Bo-Blog Official Site">Bo-Blog</a></li><li class="indent"><a href="http://www.bmforum.com" target="_blank" title="BMForum - BMForum - {langcopy1}">BMForum</a></li></ul>
+<ul><li><strong>默认链接组</strong></li><li class="indent"><a href="http://www.bo-blog.com" target="_blank" title="Bo-Blog - Bo-Blog旧官网地址，已经停止开发">Bo-Blog</a></li><li class="indent"><a href="http://forum.y48.net/forum.php?mod=forumdisplay&fid=104" target="_blank" title="问题上报地址 - 问题上报地址">问题上报地址</a></li><li class="indent"><a href="https://github.com/guoqiang5277/boblog" target="_blank" title="代码下载地址 - 代码下载地址">代码下载地址</a></li></ul>
 eot;
 ?>
