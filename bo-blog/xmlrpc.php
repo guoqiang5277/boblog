dadd5565c96b038a1643531d00c8494775c0a3fb
guoqiang5277 <guoqiang5277@163.com>
2023-12-21 20:38:43 +0800

    [fix] 修正中文乱码

--- bo-blog/xmlrpc.php
+++ bo-blog/xmlrpc.php
@@ -2,11 +2,11 @@
 /* -----------------------------------------------------
 Bo-Blog 2 : The Blog Reloaded.
 <<A Bluview Technology Product>>
-��ֹʹ��Windows���±��޸��ļ����ɴ���ɵ�һ��ʹ�ò�����ˡ�����
+禁止使用Windows记事本修改文件，由此造成的一切使用不正常恕不解答！
 PHP+MySQL blog system.
 Code: Bob Shen
 Offical site: http://www.bo-blog.com
-Copyright (c) Bob Shen �й����Ϻ�
+Copyright (c) Bob Shen 中国－上海
 In memory of my university life
 ------------------------------------------------------- */
 
